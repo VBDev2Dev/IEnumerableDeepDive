0ba8b1865a29a53cd963970f0d328a5d04c28454
Jay Asbury <vbjay.net@gmail.com>
2018-02-20 05:08:42 -0500

    Add another layer
    Shows that it is processing on the fly

--- IEnumerable Deep Dive/Form1.vb
+++ IEnumerable Deep Dive/Form1.vb
@@ -11,14 +11,17 @@
         While True
             Yield rand.Next(0, MaxValue)
         End While
+    End Function
 
-
-
+    Iterator Function OnlyEven(source As IEnumerable(Of Integer)) As IEnumerable(Of Integer)
+        For Each num In source
+            If num Mod 2 = 0 Then Yield num
+        Next
     End Function
     Private Sub btn20Random_Click(sender As Object, e As EventArgs) Handles btn20Random.Click
         Dim numbers As IEnumerable(Of Integer) = EndlessRandom(901).Take(20) ' lazy.  Not even processed yet
 
-        numbers = numbers.ToArray 'step into this line.  See how it works.
+        numbers = OnlyEven(numbers).ToArray 'step into this line.  See how it works.
 
         lstResults.Items.Clear()
         lstResults.DataSource = numbers
